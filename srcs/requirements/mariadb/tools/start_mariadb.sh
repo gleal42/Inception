32206630560469127b0a9593107a2cda99d83fbd
gleal42 <gonaloleal@gmail.com>
2023-05-02 02:19:25 +0100

    php7.4, safe instalation mariadb config files

--- srcs/requirements/mariadb/tools/start_mariadb.sh
+++ srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -2,19 +2,39 @@
 
 set -eux
 
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then 
+    mysql_install_db --datadir=/var/lib/mysql
+	echo "Database already exists"
+else
+
 service mysql start;
 
-mysql -u${MYSQL_ROOT_USER} << HERE
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} /*\!40100 DEFAULT CHARACTER SET utf8 */;
+# first line break is important (root password before configuration)
+/usr/bin/mysql_secure_installation << HERE
+
+Y
+$MYSQL_ROOT_PASSWORD
+$MYSQL_ROOT_PASSWORD
+Y
+n
+Y
+Y
 HERE
 
-mysql << HERE
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} << HERE
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} /*\!40100 DEFAULT CHARACTER SET utf8 */;
 GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 HERE
 
-service mysql stop;
+# service mysql status
+mysqladmin -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} status;
+service mysql stop
 
-mysqld --bind-address=0.0.0.0;
+fi
+
+mysqld --help --verbose | grep bootstrap
+
+mysqld
--- srcs/requirements/wordpress/tools/start_wordpress.sh
+++ srcs/requirements/wordpress/tools/start_wordpress.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eux
 
-service php7.3-fpm start
-service php7.3-fpm stop
+service php7.4-fpm start
+service php7.4-fpm stop
 
-php-fpm7.3 -F -R
+php-fpm7.4 -F -R
